e57b110d7075c8be325a814ed86d8d38f00e9534
Prathamesh Tibile <prathameshtibile1997@gmail.com>
2021-06-29 19:51:52 +0530

    enqueue is added

---
+++ Runner3.java
@@ -0,0 +1,11 @@
+package Stack_Queue;
+
+public class Runner3 {
+	public static void main(String[] args) {
+		Queue3<Integer> queue = new Queue3<Integer>();
+		queue.enqueue(56);
+		queue.enqueue(30);
+		queue.enqueue(70);
+		queue.show();
+	}
+}
