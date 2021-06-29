691b78e841c33ffd57fce72b0e9666d3da207228
Prathamesh Tibile <prathameshtibile1997@gmail.com>
2021-06-29 19:40:21 +0530

    Stack of 56,30,70 is created

---
+++ Runner.java
@@ -0,0 +1,11 @@
+package Stack_Queue;
+
+public class Runner {
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		stack.show();
+	}
+}
